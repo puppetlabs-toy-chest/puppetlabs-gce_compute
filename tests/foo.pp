ef0ab1689562b28fa014a6fd75f540862dbf1f8e
Dan Bode <dan@puppetlabs.com>
2012-09-08 09:21:49 -0700

    update test manifest

--- tests/foo.pp
+++ tests/foo.pp
@@ -3,6 +3,7 @@ define test_gce($ensure = present) {
   # need to delete everything the revers order its created in
   if ($ensure == 'absent') {
     Gce_instance["${name}1"] -> Gce_network["${name}network"]
+    Gce_instance["${name}2"] -> Gce_network["${name}network"]
     Gce_instance["${name}1"] -> Gce_disk["${name}disk"]
     Gce_firewall["${name}sshfirewall"] -> Gce_network["${name}network"]
     Gce_firewall["${name}httpfirewall"] -> Gce_network["${name}network"]
@@ -12,6 +13,7 @@ define test_gce($ensure = present) {
     zone    => 'us-central1-a',
     machine => 'n1-standard-1',
     image   => 'projects/google/images/ubuntu-12-04-v20120621',
+    network => "${name}network",
   }
 
   Gce_disk {
@@ -54,8 +56,11 @@ define test_gce($ensure = present) {
     ensure      => $ensure,
     description => 'a test VM',
     disk        => "${name}disk",
-    network     => "${name}network",
-    tags        => [$name, 'one']
+    modules     => ['puppetlabs-mysql'],
+    classes     => {
+      'mysql::server' => { 'bind_address' => '0.0.0.0' }
+    },
+    #tags        => [$name, 'one']
     # authorized_ssh_keys
     # external_ip_address
     # internal_ip_address
@@ -64,11 +69,26 @@ define test_gce($ensure = present) {
   }
 
   gce_instance { "${name}2":
-    ensure      => $ensure,
-    description => 'another test VM',
-    tag         => 'two',
+    ensure       => $ensure,
+    description  => 'another test VM',
+    module_repos => {
+      'git://github.com/bodepd/puppet-mediawiki' => 'mediawiki'
+    },
+    classes      => {
+      'mediawiki' => {
+        'server_name'      => '',
+        'admin_email'      => 'admin_email@domain.com',
+        'db_root_password' => 'root_password',
+        'instances'        => {
+          'dans_wiki' =>
+            { 'db_password' => 'db_pw', 'db_server' => "Gce_instance[${name}1][external_ip_address]" }
+        }
+      }
+    },
+    #tag          => 'two',
+    require      => Gce_instance["${name}1"],
   }
 }
 test_gce { 'duder':
-  ensure => present
+  ensure => absent
 }
