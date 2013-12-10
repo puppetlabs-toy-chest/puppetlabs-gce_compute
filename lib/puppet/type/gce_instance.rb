656224ac83af7d3e75ad442ca81f9f1191ef319e
Ryan Coleman <ryan@puppetlabs.com>
2013-12-10 13:26:54 -0800

    Update docs for change in module_repos
    Commit 9e8d9d6 swapped the behavior of the module_repos parameter
    to expect localdir => repo instead of repo => localdir. This is
    purely stylistic to conform with how resources traditionally handle
    the relationship between local and remote data.

--- lib/puppet/type/gce_instance.rb
+++ lib/puppet/type/gce_instance.rb
@@ -161,7 +161,7 @@ Puppet::Type.newtype(:gce_instance) do
   end
 
   newparam(:module_repos) do
-    desc 'Hash of module repos (repo -> localdir) to be downloaded from github.'
+    desc 'Hash of module repos (localdir => repo) to be downloaded from github. Ex. apache => git@github.com:puppetlabs/puppetlabs-apache.git'
     defaultto ''
     validate do |v|
       raise(Puppet::Error, "module_repos expects a Hash.") unless(v.is_a?(Hash) || v.empty?)
