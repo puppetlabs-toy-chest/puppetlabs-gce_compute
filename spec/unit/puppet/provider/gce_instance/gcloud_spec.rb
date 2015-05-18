23ee4cd68940d399ae2516e803c70f2cf824f86e
Isaac Hollander McCreery <ihmccreery@google.com>
2015-05-18 22:04:04 +0000

    Refactor unit tests for gce_instance provider

--- spec/unit/puppet/provider/gce_instance/gcloud_spec.rb
+++ spec/unit/puppet/provider/gce_instance/gcloud_spec.rb
@@ -1,70 +1,95 @@
 require 'spec_helper'
 
+RSpec.shared_examples "an instance that returns nil when created" do
+  it "should return nil when a resource is created" do
+    expect(provider).to receive(:gcloud).with(*gcloud_base_params + gcloud_additional_params)
+    expect(provider.create).to be_nil
+  end
+end
+
 describe Puppet::Type.type(:gce_instance).provider(:gcloud) do
-  let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                        :zone => 'us-central1-a') }
+  let(:base_params) { {:name => 'name', :zone => 'us-central1-a'} }
+  let(:additional_params) { {} }
+  let(:resource) { Puppet::Type.type(:gce_instance).new(base_params.merge(additional_params)) }
   let(:provider) { resource.provider }
-  let(:required_params) { ['compute', 'instances', 'create', 'name', '--zone', 'us-central1-a'] }
+  let(:gcloud_base_params) { ['compute', 'instances', 'create', 'name', '--zone', 'us-central1-a'] }
+  let(:gcloud_additional_params) { [] }
   let(:startup_script_file) { File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', '..', 'files', "#{resource[:startup_script]}")) }
   let(:puppet_manifest_file) { File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', '..', 'files', "#{resource[:puppet_manifest]}")) }
 
   describe "create" do
     it "should return nil when a resource is created" do
-      expect(provider).to receive(:gcloud).with(*required_params)
+      expect(provider).to receive(:gcloud).with(*gcloud_base_params)
       expect(provider.create).to be_nil
     end
 
     it "should raise an exception when the resource already exists" do
-      expect(provider).to receive(:gcloud).with(*required_params).and_raise(Puppet::ExecutionFailure.new(''))
+      expect(provider).to receive(:gcloud).with(*gcloud_base_params).and_raise(Puppet::ExecutionFailure.new(''))
       expect { provider.create }.to raise_error(Puppet::ExecutionFailure)
     end
   end
 
   context "with can_ip_forward" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :can_ip_forward => true) }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--can-ip-forward'])
-        expect(provider.create).to be_nil
-      end
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:can_ip_forward => true} }
+      let(:gcloud_additional_params) { ['--can-ip-forward'] }
     end
   end
 
   context "with boot_disk" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :boot_disk => 'disk') }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--disk', 'name=disk', 'boot=yes'])
-        expect(provider.create).to be_nil
-      end
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:boot_disk => 'disk'} }
+      let(:gcloud_additional_params) { ['--disk', 'name=disk', 'boot=yes'] }
     end
   end
 
   context "with metadata" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :metadata => {'test-metadata-key' => 'test-metadata-value'}) }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata', 'test-metadata-key=test-metadata-value'])
-        expect(provider.create).to be_nil
-      end
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:metadata => {'test-metadata-key' => 'test-metadata-value'}} }
+      let(:gcloud_additional_params) { ['--metadata', 'test-metadata-key=test-metadata-value'] }
     end
   end
 
   context "with startup_script" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :startup_script => '../examples/gce_instance/example-startup-script.sh') }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata-from-file', "startup-script=#{startup_script_file}"])
-        expect(provider.create).to be_nil
-      end
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:startup_script => '../examples/gce_instance/example-startup-script.sh'} }
+      let(:gcloud_additional_params) { ['--metadata-from-file', "startup-script=#{startup_script_file}"] }
+    end
+  end
+
+  context "with puppet_master" do
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:puppet_master => 'master-blaster'} }
+      let(:gcloud_additional_params) { ['--metadata', 'puppet_master=master-blaster'] }
+    end
+  end
+
+  context "with puppet_service" do
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:puppet_service => 'present'} }
+      let(:gcloud_additional_params) { ['--metadata', 'puppet_service=present'] }
+    end
+  end
+
+  context "with puppet_manifest" do
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:puppet_manifest => '../examples/gce_instance/example-puppet-manifest.pp'} }
+      let(:gcloud_additional_params) { ['--metadata-from-file', "puppet_manifest=#{puppet_manifest_file}"] }
+    end
+  end
+
+  context "with puppet_modules" do
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:puppet_modules => ['puppetlabs-gce_compute','puppetlabs-mysql']} }
+      let(:gcloud_additional_params) { ['--metadata', "puppet_modules=puppetlabs-gce_compute puppetlabs-mysql"] }
+    end
+  end
+
+  context "with puppet_module_repos" do
+    it_behaves_like "an instance that returns nil when created" do
+      let(:additional_params) { {:puppet_module_repos => {'puppetlabs-gce_compute' => 'git://github.com/puppetlabs/puppetlabs-gce_compute',
+                                                          'puppetlabs-mysql' => 'git://github.com/puppetlabs/puppetlabs-mysql'}} }
+      let(:gcloud_additional_params) { ['--metadata', "puppet_module_repos=git://github.com/puppetlabs/puppetlabs-gce_compute#puppetlabs-gce_compute git://github.com/puppetlabs/puppetlabs-mysql#puppetlabs-mysql"] }
     end
   end
 
@@ -77,7 +102,7 @@ describe Puppet::Type.type(:gce_instance).provider(:gcloud) do
 
     describe "create" do
       it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata-from-file', "startup-script=#{startup_script_file}"])
+        expect(provider).to receive(:gcloud).with(*gcloud_base_params + ['--metadata-from-file', "startup-script=#{startup_script_file}"])
         expect(provider).to receive(:gcloud).with(*ssh_params).exactly(3).times.and_return('Not done', 'Still not done', 'Finished running startup script')
         expect(provider).to receive(:sleep).exactly(2).times.and_return(nil)
         expect(provider.create).to be_nil
@@ -92,71 +117,10 @@ describe Puppet::Type.type(:gce_instance).provider(:gcloud) do
                                                             :startup_script_timeout => 0.001) }
       describe "create" do
         it "should raise an error when a resource is created because the timeout is reached" do
-          expect(provider).to receive(:gcloud).with(*required_params + ['--metadata-from-file', "startup-script=#{startup_script_file}"])
+          expect(provider).to receive(:gcloud).with(*gcloud_base_params + ['--metadata-from-file', "startup-script=#{startup_script_file}"])
           expect(provider).to receive(:gcloud).with(*ssh_params) { sleep 1 }
           expect { provider.create }.to raise_error(Puppet::Error)
         end
-      end
-    end
-  end
-
-  context "with puppet_master" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :puppet_master => 'master-blaster') }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata', 'puppet_master=master-blaster'])
-        expect(provider.create).to be_nil
-      end
-    end
-  end
-
-  context "with puppet_service" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :puppet_service => 'present') }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata', 'puppet_service=present'])
-        expect(provider.create).to be_nil
-      end
-    end
-  end
-
-  context "with puppet_manifest" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :puppet_manifest => '../examples/gce_instance/example-puppet-manifest.pp') }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata-from-file', "puppet_manifest=#{puppet_manifest_file}"])
-        expect(provider.create).to be_nil
-      end
-    end
-  end
-
-  context "with puppet_modules" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :puppet_modules => ['puppetlabs-gce_compute','puppetlabs-mysql']) }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata', "puppet_modules=puppetlabs-gce_compute puppetlabs-mysql"])
-        expect(provider.create).to be_nil
-      end
-    end
-  end
-
-  context "with puppet_module_repos" do
-    let(:resource) { Puppet::Type.type(:gce_instance).new(:name => 'name',
-                                                          :zone => 'us-central1-a',
-                                                          :puppet_module_repos => {'puppetlabs-gce_compute' => 'git://github.com/puppetlabs/puppetlabs-gce_compute',
-                                                                                   'puppetlabs-mysql' => 'git://github.com/puppetlabs/puppetlabs-mysql'}) }
-    describe "create" do
-      it "should return nil when a resource is created" do
-        expect(provider).to receive(:gcloud).with(*required_params + ['--metadata', "puppet_module_repos=git://github.com/puppetlabs/puppetlabs-gce_compute#puppetlabs-gce_compute git://github.com/puppetlabs/puppetlabs-mysql#puppetlabs-mysql"])
-        expect(provider.create).to be_nil
       end
     end
   end
