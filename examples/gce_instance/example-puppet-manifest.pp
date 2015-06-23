ce66f99dc922991a74ef16cfcd2f069ec60bbdf8
Isaac Hollander McCreery <ihmccreery@google.com>
2015-06-23 21:17:02 +0000

    More sane example-puppet-manifest.pp

--- examples/gce_instance/example-puppet-manifest.pp
+++ examples/gce_instance/example-puppet-manifest.pp
@@ -1,16 +1,14 @@
-class apache ($version = "latest") {
-  package {"apache2":
-    ensure => $version, # Using the class parameter from above
-  }
-  file {"/var/www/index.html":
-    ensure  => present,
-    content => "<html>\n<body>\n\t<h2>Hi, this is $gce_external_ip.</h2>\n</body>\n</html>\n",
-    require => Package["apache2"],
-  }
-  service {"apache2":
-    ensure => running,
-    enable => true,
-    require => File["/var/www/index.html"],
-  }
+# execute 'apt-get update'
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
 }
-include apache
+
+# install apache2 package
+package { 'apache2':
+  ensure  => installed,
+  require => Exec['apt-update']
+}
+
+service { 'apache2':
+  ensure => running
+}
