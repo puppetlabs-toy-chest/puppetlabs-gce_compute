63a13674686ca8bd69cdb0764af4d1a90b9f5675
Isaac Hollander McCreery <ihmccreery@google.com>
2015-06-24 22:49:28 +0000

    Remove outdated tests

--- tests/down-pe3-wheezy.pp
+++
@@ -1,9 +0,0 @@
-gce_instance { 'pe3-wheezy':
-    ensure       => absent,
-    zone         => 'us-central1-f',
-}
-gce_disk { 'pe3-wheezy':
-    ensure       => absent,
-    zone         => 'us-central1-f',
-}
-Gce_instance["pe3-wheezy"] -> Gce_disk["pe3-wheezy"]
